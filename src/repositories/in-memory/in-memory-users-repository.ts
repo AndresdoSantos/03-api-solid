f787f78fb15630aee03140c9824c435ab789785d
AndresdoSantos <andres.dosantosbritoamaral@gmail.com>
2023-04-25 15:01:59 -0300

    test: finishing register unity tests

---
+++ src/repositories/in-memory/in-memory-users-repository.ts
@@ -0,0 +1,31 @@
+import { Prisma, User } from '@prisma/client'
+
+import { IUserRepository } from '../users-repository'
+
+export class InMemoryUsersRepository implements IUserRepository {
+  public items: User[] = []
+
+  async findByEmail(email: string) {
+    const user = this.items.find((item) => item.email === email)
+
+    if (!user) {
+      return null
+    }
+
+    return user
+  }
+
+  async create(data: Prisma.UserCreateInput) {
+    const user = {
+      id: 'user-1',
+      email: data.email,
+      name: data.name,
+      passwordHash: data.passwordHash,
+      createdAt: new Date(),
+    }
+
+    this.items.push(user)
+
+    return user
+  }
+}
