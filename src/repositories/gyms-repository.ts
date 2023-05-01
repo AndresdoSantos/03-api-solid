457388b519aacfb5ac68560243b775d234bca535
AndresdoSantos <andres.dosantosbritoamaral@gmail.com>
2023-05-01 16:55:30 -0300

    feat(use-cases/fetch-nearby-gyms.ts): implementing use case to search nearby gyms

--- src/repositories/gyms-repository.ts
+++ src/repositories/gyms-repository.ts
@@ -1,9 +1,19 @@
 import { Gym, Prisma } from '@prisma/client'
 
+export interface IFindManyNearbyManyParams {
+  latitude: number
+  longitude: number
+}
+
 export interface IGymsRepository {
   findById(id: string): Promise<Gym | null>
 
   searchMany(query: string, page: number): Promise<Gym[]>
+
+  findManyNearby({
+    latitude,
+    longitude,
+  }: IFindManyNearbyManyParams): Promise<Gym[]>
 
   create(data: Prisma.GymCreateInput): Promise<Gym>
 }
--- src/repositories/in-memory/in-memory-gyms-repository.ts
+++ src/repositories/in-memory/in-memory-gyms-repository.ts
@@ -1,10 +1,26 @@
 import { Gym, Prisma } from '@prisma/client'
 import { randomUUID } from 'node:crypto'
 
-import { IGymsRepository } from '../gyms-repository'
+import { IFindManyNearbyManyParams, IGymsRepository } from '../gyms-repository'
+
+import { getDistanceBetweenCoordinates } from '@/utils/get-distance-between-coordinates'
 
 export class InMemoryGymsRepository implements IGymsRepository {
   public items: Gym[] = []
+
+  async findManyNearby({ latitude, longitude }: IFindManyNearbyManyParams) {
+    return this.items.filter((item) => {
+      const distance = getDistanceBetweenCoordinates(
+        { latitude, longitude },
+        {
+          latitude: item.latitude.toNumber(),
+          longitude: item.longitude.toNumber(),
+        },
+      )
+
+      return distance < 10
+    })
+  }
 
   async searchMany(query: string, page: number) {
     return this.items
